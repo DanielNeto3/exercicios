b1d181aa00e022ce5a59c2d77a33a5c3934f44a8
DanielNeto3 <133465523+DanielNeto3@users.noreply.github.com>
2025-11-04 14:17:22 -0300

    Add files via upload

---
+++ questões/1019/1019/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int H, hora, minuto, segundo;
+        H = int.Parse(Console.ReadLine());
+
+        hora = H / 3600;
+        minuto = (H % 3600) / 60;
+        segundo = H % 60;
+
+        Console.WriteLine(hora + ":" + minuto + ":" + segundo);
+    }
+
+}
