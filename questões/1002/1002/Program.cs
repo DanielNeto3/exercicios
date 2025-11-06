b281c5073eba9bf6fdbe3e5613533010df409607
DanielNeto3 <133465523+DanielNeto3@users.noreply.github.com>
2025-11-06 20:28:07 -0300

    Add files via upload

---
+++ questões/1002/1002/Program.cs
@@ -0,0 +1,18 @@
+﻿using System; 
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        double raio, pi, area;
+        raio = double.Parse(Console.ReadLine());
+        pi = 3.14159;
+        area = pi * (raio * raio);
+
+        Console.WriteLine("A=" + area.ToString("f4"));
+
+
+    }
+
+}
