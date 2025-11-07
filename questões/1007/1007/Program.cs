f3d6550cb8888cae3c3d4e72c874349d45a4a7f2
DanielNeto3 <133465523+DanielNeto3@users.noreply.github.com>
2025-11-06 21:34:13 -0300

    Add files via upload

---
+++ questões/1007/1007/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int a, b, c, d, dif;
+
+        a = int.Parse(Console.ReadLine());
+        b = int.Parse(Console.ReadLine());
+        c = int.Parse(Console.ReadLine());
+        d = int.Parse(Console.ReadLine());
+
+        dif = a * b - c * d;
+
+        Console.WriteLine("DIFERENCA = " + dif);
+
+
+
+
+
+
+
+    }
+
+}
---
+++ questões/1008/1008/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int fun, hora;
+        double sal, salt;
+
+        fun = int.Parse(Console.ReadLine());
+        hora = int.Parse(Console.ReadLine());
+        sal = double.Parse(Console.ReadLine());
+
+        salt = hora * sal;
+
+        Console.WriteLine("NUMBER = " + fun);
+        Console.WriteLine("SALARY = U$ " + salt.ToString("f2"));
+
+
+
+
+
+
+
+    }
+
+}
