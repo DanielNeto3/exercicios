6b72229df982839a48d9d98aed7735c00b30df88
DanielNeto3 <133465523+DanielNeto3@users.noreply.github.com>
2025-11-09 05:10:29 -0300

    Add files via upload

---
+++ questões/1014/1014/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int dtp;
+        double tcg, tdcg;
+
+        dtp = int.Parse(Console.ReadLine());
+        tcg = double.Parse(Console.ReadLine());
+
+        tdcg = dtp / tcg;
+
+        Console.WriteLine(tdcg.ToString("F3") + " km/l");
+
+        
+
+    }
+
+}
---
+++ questões/1016/1016/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int tdd, tdc;
+
+        tdd = int.Parse(Console.ReadLine());
+
+        tdc = tdd * 2;
+
+        Console.WriteLine(tdc + " minutos");
+
+
+
+    }
+
+}
