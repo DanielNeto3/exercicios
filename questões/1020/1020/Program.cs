8168c356f3fd238f31dea68802d8e1162a4cd6a9
DanielNeto3 <133465523+DanielNeto3@users.noreply.github.com>
2025-12-06 23:33:46 -0300

    Add files via upload

---
+++ questões/1020/1020/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int N, resto, ano, mes, dias;
+        N = int.Parse(Console.ReadLine());
+     
+        ano = N / 365;
+        resto = N % 365;
+        Console.WriteLine(ano + " ano(s)");
+
+        mes = resto / 30;
+        Console.WriteLine(mes + " mes(es)");
+        resto = resto % 30;
+
+        dias = resto / 1;
+        Console.WriteLine(dias + " dia(s)");
+
+
+
+
+    }
+
+}
